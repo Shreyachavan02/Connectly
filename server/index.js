442e62db93d7076c52749c49cfbdc83c66748cf8
Shreyachavan02 <chavanshreya654@gmail.com>
2025-11-14 22:36:50 +0530

    server hosted on render

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ let requestCount = 0;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully:", conn.connection.host);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
